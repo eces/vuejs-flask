cf03ad81e8b25937c668478dba01ea59673fd1a4
Jinhyuk Lee <jin@playauto.co.kr>
2018-03-13 15:46:25 +0900

    Init alembic

---
+++ api/alembic/versions/3f0d801c9a5b_create_user_table.py
@@ -0,0 +1,24 @@
+"""create user table
+
+Revision ID: 3f0d801c9a5b
+Revises: 
+Create Date: 2018-03-13 11:56:15.041342
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '3f0d801c9a5b'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
